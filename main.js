bc4725d597e8fccc20a87c56411d96a95175ddff
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-06 17:08:26 +1100

    completed lab

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const glyphStates = {
+  '♡': '♥',
+  '♥': '♡'
+};
+
+const colorStates = {
+  'red': '',  // You might need to adjust this based on your initial styling
+  '': 'red'
+};
+
+
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert("You notified the server!");
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
 
 
-
+for (const glyph of articleHearts) {
+   glyph.addEventListener("click", likeCallback);
+ }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
